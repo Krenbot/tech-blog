dbbfa9c29fe44e2ee3e4d18e8ed5096b875478dd
Steve Krenn <stevekrenn@gmail.com>
2022-12-13 13:52:01 -0600

    added prototype todos in blog/comment routes

---
+++ controllers/api/blogRoutes.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+const { Blog } = require("../../models")
+
+//GET all
+//GET one
+//CREATE new (.post)
+//UPDATE (.put)
+//DELETE (.delete)
+
+
+module.exports = router;
---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const { Comment } = require("../../models")
+
+//GET all
+//GET one
+//CREATE new (.post)
+//UPDATE (.put)
+//DELETE (.delete)
+
+
+
+
+
+
+module.exports = router;
