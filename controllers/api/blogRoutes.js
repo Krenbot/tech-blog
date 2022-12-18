4827b320a3d72047eb9d05504676569d95d7c6dd
Steven Krenn <stevekrenn@gmail.com>
2022-12-18 17:27:05 -0600

    added prototype routes in blog api

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -2,8 +2,34 @@ const router = require("express").Router();
 const { Blog } = require("../../models")
 
 //CREATE (.post /)
+router.post('/', async (req, res) => {
+    try {
+
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 //UPDATE (.put /:id)
+router.put('/:id', async (req, res) => {
+    try {
+
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
+
 //DELETE (.delete /:id)
+router.delete('/:id', async (req, res) => {
+    try {
+
+
+    } catch (err) {
+        res.status(400).json(err)
+    }
+})
 
 
 module.exports = router;
