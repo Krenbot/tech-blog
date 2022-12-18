84611332149b14bbc76cfe98254bb5cb62e0a6ab
Steven Krenn <stevekrenn@gmail.com>
2022-12-18 17:17:47 -0600

    added test user in seeds, reworked login/signup logic

--- seeds/userData.js
+++ seeds/userData.js
@@ -3,9 +3,9 @@ const { User } = require('../models')
 const userData =
   [
     {
-      "name": "Sal",
-      "email": "sal@hotmail.com",
-      "password": "password12345"
+      "name": "test",
+      "email": "test@test.com",
+      "password": "test"
     },
     {
       "name": "Lernantino",
